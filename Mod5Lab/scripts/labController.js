6f20623f43b7110128e59a0a89c347d16ceb5e46
Brian Hendel <brian.hendel@gmail.com>
2016-12-01 19:24:07 -0500

    Adds modules 4 and 5 and completes course

---
+++ Mod5Lab/scripts/labController.js
@@ -0,0 +1,20 @@
+app.controller('labController', [
+    '$scope',
+    function ($scope) {
+
+        $scope.reset = reset;
+
+        reset();
+
+        function reset() {
+            $scope.model = {};
+        }
+
+        $scope.submit = submit;
+
+        function submit(model) {
+            alert("Submitted:" + '\n' + JSON.stringify(model));
+        }
+
+    }
+]);
